44cdc87ee87ae546d4028a0e360770bfbe9edc60
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-09-03 14:17:17 +0600

    index.js file changed

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.get("/", () => console.log("Hello World!"))
+app.get("/", (req, res) => res.send({message: "Hello World!"}))
 app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`))
